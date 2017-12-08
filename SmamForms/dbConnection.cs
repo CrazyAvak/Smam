96e3f8a20fd5bd8edd1a8a725a9f620006f605d5
crazyavak <lars.boerboom@outlook.com>
2017-12-08 09:43:32 +0100

    - changed to windows forms

---
+++ SmamForms/dbConnection.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmamForms
+{
+    class dbConnection
+    {
+        MySqlConnection conn;
+        public dbConnection()
+        {
+            string connectionString = "Server=localhost;Database=seriesreminder;Uid=root;Pwd=;";
+            conn = new MySqlConnection(connectionString);
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("not connected");
+            }
+        }
+
+        
+    }
+}
