e0259f9a0506473b1b25698a9b97f38a2b38bd0c
crazyavak <lars.boerboom@outlook.com>
2017-12-20 12:57:30 +0100

    - added logo

--- SmamForms/SplashScreenForm.Designer.cs
+++ SmamForms/SplashScreenForm.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.buttonNext = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonNext
@@ -43,14 +45,26 @@
             this.buttonNext.Visible = false;
             this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SmamForms.Properties.Resources.Artboard_1;
+            this.pictureBox1.Location = new System.Drawing.Point(46, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(245, 241);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
             // SplashScreenForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(339, 611);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonNext);
             this.Name = "SplashScreenForm";
             this.Text = "Smam";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,5 +72,6 @@
         #endregion
 
         private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
